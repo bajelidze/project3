7469523c406bac87b8235b59debd27194984ddfc
nuriyev98 <emin.n@mail.ru>
2017-11-23 23:46:59 +0400

    Created menu design

---
+++ Tetris/Tetris/Custom.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+using System;
+
+namespace Tetris
+{
+    class Custom : Control
+    {
+        public Custom()
+        {
+            this.BackgroundImage = global::Tetris.Properties.Resources.selectedNew;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Paint += Custom_Paint;
+        }
+
+        private void Custom_Paint(object sender, PaintEventArgs e)
+        {
+           // e.Graphics.DrawString(this.Text, this.Font, 30, 30);
+        }
+    }
+}
---
+++ Tetris/Tetris/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tetris
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void play_Click(object sender, EventArgs e)
+        {
+            Control button = (Control)sender;
+            button.Visible = false;
+        }
+
+        private void custom_Enter(object sender, EventArgs e)
+        {
+            Control button = (Control)sender;
+            button.BackgroundImage = global::Tetris.Properties.Resources.selected;
+            button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+        }
+
+        private void custom_MouseLeave(object sender, EventArgs e)
+        {
+            Control button = (Control)sender;
+            button.BackgroundImage = global::Tetris.Properties.Resources.selectedNew;
+            button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+        }
+    }
+}
